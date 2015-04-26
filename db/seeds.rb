176ccfe00e9facea89ce04595e9e84b7ac8298ca
Sulman Baig <sulman1990@hotmail.com>
2015-04-26 12:21:43 +0200

    added seed database files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'Waqar', email: 'waqarbaig85@gmail.com', password: 'Helloworld', password_confirmation: 'Helloworld')
+User.create(username: 'Sulman', email: 'sulmanpersonal@gmail.com', password: 'Helloworld', password_confirmation: 'Helloworld')
+User.create(username: 'Maria', email: 'mariah.hafeez@gmail.com', password: 'Helloworld', password_confirmation: 'Helloworld')
+User.create(username: 'Umme Sulman', email: 'mrsbaig449@gmail.com', password: 'Helloworld', password_confirmation: 'Helloworld')
+User.create(username: 'Usman', email: 'usmanbaig82@gmail.com', password: 'Helloworld', password_confirmation: 'Helloworld')
+p "Seed users created."
