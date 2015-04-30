9e8e972b94adc261f8ba08d3cbdfefa1e8a64521
Sulman Baig <sulman1990@hotmail.com>
2015-04-30 14:05:57 +0200

    added posts controller, views and routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :destroy, :update]
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      respond_to do |format|
+        format.html {redirect_to user_path(@post.user.username), notice: @post.user.username+' created a post.'}
+      end
+    else
+      redirect_to user_path(@post.user.username), notice: 'Something went wrong! Please try again later'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      respond_to do |format|
+        format.html {redirect_to user_path(@post.user.username), notice: @post.user.username+' edited a post.'}
+      end
+    else
+      redirect_to edit_post_path(@post), notice: 'Something went wrong! Please try again later'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html {redirect_to user_path(@post.user.username), notice: @post.user.username+' deleted a post.'}
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
